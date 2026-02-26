6f7c842c3a6df4769debc289c4d12f08059723af
braxtonc360360 <braxtonc360360@gmail.com>
2026-02-26 09:17:46 -0500

    Fix: correct analytics import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Analytics } from '@vercel/analytics/next';\
+import { Analytics } from '@vercel/analytics/react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
